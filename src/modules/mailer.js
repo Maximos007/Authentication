d06a672ec0f4c82f245294c947bd53f9290b783f
Maximos007 <waslleymax@gmail.com>
2021-04-16 17:48:02 -0300

    Create mailer.js

---
+++ src/modules/mailer.js
@@ -0,0 +1,20 @@
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+
+const { host, port, user, pass } = require('../config/mail.json');
+
+const  transport = nodemailer.createTransport({
+	host,
+	port,
+	auth: {
+		user,
+		pass,
+	}
+});
+
+transport.use('compile', hbs({
+	viewEngine: 'handlebars',
+	viewPath: ''
+}));
+
+module.exports = transport;
